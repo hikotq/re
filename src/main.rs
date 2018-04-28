5e8a38db3d6bb1d0b1c68db03640e8e7f7f9bf61
pipopa <pipopa.710@gmail.com>
2018-04-28 10:16:33 +0900

    delete code for wasm

--- src/main.rs
+++ src/main.rs
@@ -13,46 +13,9 @@ use dfa::Dfa;
 #[macro_use]
 extern crate lazy_static;
 
-pub fn main() {}
-
-extern "C" {
-    fn print(n: String);
-}
-
-lazy_static! {
-    static ref HEAP: Mutex<Vec<u8>> = Mutex::new(Vec::new());
-}
-
-#[no_mangle]
-pub fn alloc(size: usize) -> *mut c_char {
-    let mut output = HEAP.lock().unwrap();
-    output.resize(size, 0);
-    output.as_mut_ptr() as *mut c_char
-}
-
-#[no_mangle]
-pub fn offset() -> *const c_char {
-    let mut output = HEAP.lock().unwrap();
-    output.as_mut_ptr() as *const c_char
-}
-
-#[no_mangle]
-pub fn get_heap_len() -> usize {
-    HEAP.lock().unwrap().len()
-}
-
-#[no_mangle]
-pub fn get_dot() -> *mut c_char {
-    let mut output = HEAP.lock().unwrap();
-    let regex = output
-        .clone()
-        .iter()
-        .map(|&b| b as char)
-        .collect::<String>();
+pub fn main() {
+    let regex = "ab*";
     let nfa = nfa::Nfa::re2nfa(&regex);
     let dfa = Dfa::nfa2dfa(&nfa);
-
-    output.clear();
-    write!(output, "{}\0", dfa.dot()).unwrap();
-    output.as_ptr() as *mut c_char
+    dfa.write("dot.dot");
 }
